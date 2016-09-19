b61277e4708e4042530a6c1bd70c4ed2698b7d13
jade123 <312287995@qq.com>
2016-09-19 16:43:34 +0800

    Update WebSocketConfig.java

--- src/main/java/com/robot/config/WebSocketConfig.java
+++ src/main/java/com/robot/config/WebSocketConfig.java
@@ -3,7 +3,12 @@ package com.robot.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
-
+/**
+ * Socket 配置
+ *@author panlunyou
+ *@date 2016年9月19日下午4:39:40
+ *@version 1.0
+ */
 @Configuration
 public class WebSocketConfig {
     @Bean
